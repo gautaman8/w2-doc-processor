56dbccfe34356ddf243fa466726dd1bbd23c0adc
Gautaman Sarangan <gautaman8@gmail.com>
2025-09-16 23:58:48 +0530

    Minor fix for error after model update

--- doc_processor_backend/w2_job_app/serializers.py
+++ doc_processor_backend/w2_job_app/serializers.py
@@ -4,7 +4,7 @@ from .models import W2Job
 class W2JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = W2Job
-        fields = ['id', 'job_id', 'filename', 'file_size', 'file_type', 'status', 'signed_url', 'created_at', 'updated_at']
+        fields = ['id', 'job_id', 'filename', 'file_uploaded', 'status', 'signed_url', 'created_at', 'updated_at']
         read_only_fields = ['id', 'job_id', 'created_at', 'updated_at']
 
 class CreateJobResponseSerializer(serializers.Serializer):
